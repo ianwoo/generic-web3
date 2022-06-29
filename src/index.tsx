40259cffbd07bcda672d3e682c574e8e7509a900
0xAngel <0xangel@waterfalldefi.org>
2022-06-29 17:24:10 +0800

    web3react provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import React from 'react';
+
+import { ethers } from 'ethers';
+import ReactDOM from 'react-dom/client';
+
+import { Web3ReactProvider } from '@web3-react/core';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const getLibrary = (provider: any): ethers.providers.Web3Provider => {
+  const library = new ethers.providers.Web3Provider(provider);
+  return library;
+};
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>
 );
 
